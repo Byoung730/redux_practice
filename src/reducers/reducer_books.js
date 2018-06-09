14e8ebaab02729169e38b96d546fc2a646e065af
byoung730 <brian.young@monsanto.com>
2018-06-08 20:35:42 -0500

    redux connect & container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Moby Dick' },
+    { title: 'The Dark Tower' },
+    { title: 'Shatterpoint' }
+  ]
+}
