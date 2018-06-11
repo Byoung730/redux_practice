fea3401b14ec06562db90adda7fdd8f2f4c5a1d8
byoung730 <brian.young@monsanto.com>
2018-06-11 13:06:25 -0500

    Merge branch 'my_work'
    Conflicts:
    	src/actions/index.js
    	src/components/app.js
    	src/index.js
    	src/reducers/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -14,3 +15,53 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('.container')
 )
+=======
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/searchBar'
+import YTSearch from 'youtube-api-search'
+import VideoDetail from './components/videoDetail'
+import VideoList from './components/videoList'
+import _ from 'lodash'
+
+const API_KEY = 'AIzaSyCi7NOO1Tko2Aymdo5ZXInsDcX7f5JsV2g'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+    this.videoSearch('kobe')
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term)
+    }, 300)
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
+>>>>>>> master
